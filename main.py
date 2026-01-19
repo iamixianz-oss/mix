3d4e50689379eba41f900cc966aa171bcd542d2d
Micko <micko@gmail.com>
2026-01-19 23:33:45 +0800

    laptop

--- main.py
+++ main.py
@@ -761,4 +761,4 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=Tue)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
